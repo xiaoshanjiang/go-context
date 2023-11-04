70bd9560fc41a8007aa306afe12e417fc3c1fe20
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2023-11-04 14:53:41 +1100

    timeout cancellation

--- main.go
+++ main.go
@@ -6,19 +6,6 @@ import (
 	"time"
 )
 
-func worker(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Recieved cancellation signal, task is ended")
-			return
-		default:
-			fmt.Println("Executing task...")
-			time.Sleep(1 * time.Second)
-		}
-	}
-}
-
 func main() {
 	// create an empty Context
 	ctx := context.Background()
@@ -27,9 +14,21 @@ func main() {
 	ctxWithCancel, cancel := context.WithCancel(ctx)
 	defer cancel() // defer cancel, make sure Context is cancelled when existing the function
 
-	go worker(ctxWithCancel)
+	go func() {
+		for {
+			select {
+			case <-ctxWithCancel.Done():
+				fmt.Println("Recieved cancellation signal, task is ended")
+				return
+			// simulate a long-running task and prints out "Task done"
+			// if the task is done within 2 seconds
+			case <-time.After(2 * time.Second):
+				fmt.Println("Task done")
+			}
+		}
+	}()
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(1 * time.Second)
 	cancel() // send cancellation signal
 	time.Sleep(1 * time.Second)
 }
