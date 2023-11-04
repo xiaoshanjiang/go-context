674f3b24435db8931a411e1ae20f32582694ff91
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2023-11-04 14:40:52 +1100

    created a context with cancellation signal and passed it to another goroutine

--- main.go
+++ main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
+
+func worker(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Recieved cancellation signal, task is ended")
+			return
+		default:
+			fmt.Println("Executing task...")
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
 
 func main() {
 	// create an empty Context
 	ctx := context.Background()
 
 	// create a Context with cancelling signal
-	_, cancel := context.WithCancel(ctx)
+	ctxWithCancel, cancel := context.WithCancel(ctx)
 	defer cancel() // defer cancel, make sure Context is cancelled when existing the function
 
-	fmt.Println("Context created successfully.")
+	go worker(ctxWithCancel)
+
+	time.Sleep(3 * time.Second)
+	cancel() // send cancellation signal
+	time.Sleep(1 * time.Second)
 }
